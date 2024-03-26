dd732c9c1790a6d1be0efcd5dfeeb6f699dc0896
gla1 <gene@langfamily.info>
2024-03-26 07:34:38 -0500

    5.4 Double Cliffs

--- Assets/Scripts/HexMesh.cs
+++ Assets/Scripts/HexMesh.cs
@@ -159,7 +159,18 @@ public class HexMesh : MonoBehaviour {
             TriangulateCornerCliffTerraces(bottom, bottomCell, left, leftCell, right, rightCell);
             return;
         }
-        
+
+        if (leftCell.GetEdgeType(rightCell) == HexEdgeType.Slope)
+        {
+            if (leftCell.Elevation < rightCell.Elevation)
+            {
+                TriangulateCornerCliffTerraces(right, rightCell, bottom, bottomCell, left, leftCell);
+            }
+            else
+            {
+                TriangulateCornerTerracesCliff(left, leftCell, right, rightCell, bottom, bottomCell);
+            }
+        }
         AddTriangle(bottom, left, right);
         AddTriangleColor(bottomCell.color, leftCell.color, rightCell.color);
     }
@@ -195,6 +206,10 @@ public class HexMesh : MonoBehaviour {
         HexCell rightCell)
     {
         float b = 1f / (rightCell.Elevation - beginCell.Elevation);
+        if (b < 0)
+        {
+            b = -b;
+        }
         Vector3 boundary = Vector3.Lerp(begin, right, b);
         Color boundaryColor = Color.Lerp(beginCell.color, rightCell.color, b);
         
@@ -215,6 +230,10 @@ public class HexMesh : MonoBehaviour {
         HexCell rightCell)
     {
         float b = 1f / (leftCell.Elevation - beginCell.Elevation);
+        if (b < 0)
+        {
+            b = -b;
+        }
         Vector3 boundary = Vector3.Lerp(begin, left, b);
         Color boundaryColor = Color.Lerp(beginCell.color, leftCell.color, b);
         
