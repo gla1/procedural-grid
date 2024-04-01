a78fdeaab35045d5e9672af6df849c8db5da2e03
gla1 <gene@langfamily.info>
2024-04-01 15:53:51 -0500

    5 Reconnecting Cliffs and Terraces

--- Assets/Scripts/HexMesh.cs
+++ Assets/Scripts/HexMesh.cs
@@ -48,6 +48,17 @@ public class HexMesh : MonoBehaviour {
         triangles.Add(vertexIndex +2);
     }
 
+    void AddTriangleUnperturbed(Vector3 v1, Vector3 v2, Vector3 v3)
+    {
+        int vertexIndex = vertices.Count;
+        vertices.Add(v1);
+        vertices.Add(v2);
+        vertices.Add(v3);
+        triangles.Add(vertexIndex);
+        triangles.Add(vertexIndex +1);
+        triangles.Add(vertexIndex +2);
+    }
+
     void Triangulate(HexCell cell)
     {
         for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++)
@@ -230,7 +241,7 @@ public class HexMesh : MonoBehaviour {
         {
             b = -b;
         }
-        Vector3 boundary = Vector3.Lerp(begin, right, b);
+        Vector3 boundary = Vector3.Lerp(Perturb(begin), Perturb(right), b);
         Color boundaryColor = Color.Lerp(beginCell.color, rightCell.color, b);
         
         TriangulateBoundaryTriangle(begin, beginCell, left, leftCell, boundary, boundaryColor);
@@ -254,7 +265,7 @@ public class HexMesh : MonoBehaviour {
         {
             b = -b;
         }
-        Vector3 boundary = Vector3.Lerp(begin, left, b);
+        Vector3 boundary = Vector3.Lerp(Perturb(begin), Perturb(left), b);
         Color boundaryColor = Color.Lerp(beginCell.color, leftCell.color, b);
         
         TriangulateBoundaryTriangle(right, rightCell, begin, beginCell, boundary, boundaryColor);
@@ -274,23 +285,22 @@ public class HexMesh : MonoBehaviour {
     void TriangulateBoundaryTriangle(Vector3 begin, HexCell beginCell, Vector3 left, HexCell leftCell, Vector3 boundary,
         Color boundaryColor) 
     {
-
-        Vector3 v2 = HexMetrics.TerraceLerp(begin, left, 1);
+        Vector3 v2 = Perturb(HexMetrics.TerraceLerp(begin, left, 1));
         Color c2 = HexMetrics.TerraceLerp(beginCell.color, leftCell.color, 1);
-        
-        AddTriangle(begin, v2, boundary);
+
+        AddTriangleUnperturbed(Perturb(begin), v2, boundary);
         AddTriangleColor(beginCell.color, c2, boundaryColor);
-        
+
         for (int i = 2; i < HexMetrics.terraceSteps; i++) {
             Vector3 v1 = v2;
             Color c1 = c2;
-            v2 = HexMetrics.TerraceLerp(begin, left, i);
+            v2 = Perturb(HexMetrics.TerraceLerp(begin, left, i));
             c2 = HexMetrics.TerraceLerp(beginCell.color, leftCell.color, i);
-            AddTriangle(v1, v2, boundary);
+            AddTriangleUnperturbed(v1, v2, boundary);
             AddTriangleColor(c1, c2, boundaryColor);
         }
-        
-        AddTriangle(v2, left, boundary);
+
+        AddTriangleUnperturbed(v2, Perturb(left), boundary);
         AddTriangleColor(c2, leftCell.color, boundaryColor);
     }
 
