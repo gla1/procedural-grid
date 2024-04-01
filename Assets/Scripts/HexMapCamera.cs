132e0d97b48b7c8c339a6f1828a16306d25ed885
gla1 <gene@langfamily.info>
2024-04-01 17:46:08 -0500

    2 Camera Controls

---
+++ Assets/Scripts/HexMapCamera.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class HexMapCamera : MonoBehaviour
+{
+    Transform swivel, stick;
+    
+    void Awake()
+    {
+        swivel = transform.GetChild(0);
+        stick = swivel.GetChild(0);
+    }
+}
